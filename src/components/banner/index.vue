e2ce0391a62d3dcf8e8f1e9be21b666f70b2bb56
Zhang Ruoyu <1753071038@qq.com>
2018-01-25 15:21:03 +0800

    banner轮播图

--- src/components/banner/index.vue
+++ src/components/banner/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="banner" >
-  	123
+  	<mt-swipe :auto="4000">
+  <mt-swipe-item>1</mt-swipe-item>
+  <mt-swipe-item>2</mt-swipe-item>
+  <mt-swipe-item>3</mt-swipe-item>
+	</mt-swipe>
   </div>
 </template>
 <script src="./script.js"></script>
--- src/components/banner/script.js
+++ src/components/banner/script.js
@@ -1,3 +1,8 @@
+import Vue from 'vue';
+import { Swipe, SwipeItem } from 'mint-ui';
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
 export default {
 	data() {
 		return {
